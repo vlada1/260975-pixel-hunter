821eba36224ece3219d68190405be9a07e927a42
Vlada Andrushchenko <avv771kn@gmail.com>
2017-10-02 20:06:59 +0300

    Исправила объявление переменных

--- js/service.js
+++ js/service.js
@@ -1,11 +1,11 @@
 export const createDomElement = (templateContent) => {
-  let container = document.createElement(`div`);
+  const container = document.createElement(`div`);
   container.innerHTML = templateContent;
   return container;
 };
 
 export const renderPage = (element) => {
-  let mainElement = document.getElementById(`main`);
+  const mainElement = document.getElementById(`main`);
   mainElement.innerHTML = ``;
   return mainElement.appendChild(element);
 };
